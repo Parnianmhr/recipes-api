fddb567ec4ec7298b66c77fec4a6c0d56cf4bc67
Parnian <parnian@parnians-mbp.home>
2017-04-25 17:29:06 +0200

    recipes service generated

---
+++ src/services/recipe/recipe-model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// recipe-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  text: { type: String, required: true },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+const recipeModel = mongoose.model('recipe', recipeSchema);
+
+module.exports = recipeModel;
